900f6347f638c05905b40ccbb0512687616d2ab7
fkhan <fatima.khan@alumnos.unican.es>
2025-06-05 11:03:42 +0000

    ++

--- makecert.sh
+++ makecert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # call this script with an email address (valid or not).
-mkdir ./c_example/go_client/certs
-rm ./c_example/go_client/certs/*
+mkdir ./go_client/certs
+rm ./go_client/certs/*
 echo "make server cert"
 openssl req -new -nodes -x509 -out ./go_client/certs/server.pem -keyout ./go_client/certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random"
 echo "make client cert"
